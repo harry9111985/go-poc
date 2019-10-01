db313f8f279d1ec8c3420148da7967f9dec5f5cb
Harish Subramaniam <harry9.11.1985@gmail.com>
2019-10-01 14:47:59 +1000

    New exercise - Constants

---
+++ src/constants.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("Go - Constants exercise : start")
+
+	// Declaring constants with an initial value . Constants are always initialized with a value . Otherwise compiler will throw error.
+	const MinAge = 30
+
+	fmt.Println("Minimum age is ", MinAge)
+
+	const DefaultName = "XYZ"
+	type myString string
+	var name myString = DefaultName
+	someOtherName := "DEF"
+
+	fmt.Println("Some other name", someOtherName)
+
+	// This will be not be not be allowed as someOtherName has a different type and not a constant
+	//name = someOtherName
+
+	fmt.Println("Name with a custom string type initialized with Default Name", name)
+
+	fmt.Println("Go - Constants exercise : end")
+}
