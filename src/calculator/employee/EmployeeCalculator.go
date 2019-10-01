58ebf15b76c864f326da633ae375b30f1d303b3b
Harish Subramaniam <harry9.11.1985@gmail.com>
2019-10-01 21:47:32 +1000

    init function

--- src/calculator/employee/EmployeeCalculator.go
+++ src/calculator/employee/EmployeeCalculator.go
@@ -3,6 +3,8 @@
  */
 package employee
 
+import "fmt"
+
 /**
  the func outside the main package should start with a capital letter to be exposed in a main function
  */
@@ -11,3 +13,9 @@ func CalculateSalary(wagePerHour float32, noOfHoursPerDay int, noOfDays int) flo
 	return float64(wagePerHour * (float32(noOfHoursPerDay)) * float32(noOfDays))
 }
 
+/**
+This is the first function called when this package is imported as part of main package.
+ */
+func init() {
+	fmt.Println("Initializing employee calculator package")
+}
