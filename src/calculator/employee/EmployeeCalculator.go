363e5ea224850c42ec3df22ff5dfb3a3836a7ebd
Harish Subramaniam <harry9.11.1985@gmail.com>
2019-10-02 06:23:14 +1000

    init function with package variables.

--- src/calculator/employee/EmployeeCalculator.go
+++ src/calculator/employee/EmployeeCalculator.go
@@ -5,17 +5,25 @@ package employee
 
 import "fmt"
 
+
+const minWagePerHour = 15
+const minNoOfHoursPerDay = 6
+const minNoOfDays = 10
+
 /**
  the func outside the main package should start with a capital letter to be exposed in a main function
  */
 func CalculateSalary(wagePerHour float32, noOfHoursPerDay int, noOfDays int) float64 {
-
-	return float64(wagePerHour * (float32(noOfHoursPerDay)) * float32(noOfDays))
+    if wagePerHour >=minWagePerHour && noOfHoursPerDay >=minNoOfHoursPerDay && noOfDays >=minNoOfDays {
+		return float64(wagePerHour * (float32(noOfHoursPerDay)) * float32(noOfDays))
+	}else {
+		return -1
+	}
 }
 
 /**
 This is the first function called when this package is imported as part of main package.
  */
 func init() {
-	fmt.Println("Initializing employee calculator package")
+	fmt.Println("Initializing employee calculator package" , minWagePerHour,minNoOfHoursPerDay,minNoOfDays)
 }
